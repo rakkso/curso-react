0e6b4606041719e9b6670f5e74e201663e6de31c
Óscar <rak0kso@gmail.com>
2023-08-28 18:39:55 +0200

    Sección 4 Tema 47 Enendiendo el hook useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -6,20 +6,39 @@ import PropTypes from 'prop-types';
  
  export function CounterApp( { valor}) {
     
+    //console.log("Renderizando entro",contador); No funciona porque no se ha creado contador
     const [contador, actualizaContador] = useState(valor);
-
-    function mifuncion(event) {
-        console.log(event);
+    console.log("Renderizando salgo",contador);
+    function sumar(event) {
+        //console.log(event);
         //da error porque contador es una constante contador=contador +1;
+        console.log("sumar entra:",contador)
         actualizaContador(contador +1);
-        console.log(contador)
+        setTimeout(console.log("sumar queda:",contador),100);
+        
     }
-    
+    function restar(event) {
+        //console.log(event);
+        //da error porque contador es una constante contador=contador +1;
+        console.log("restar entra:",contador)
+        actualizaContador(contador -1);
+        console.log("restar queda:",contador)
+    }   
+
+    function reset(event) {
+        //console.log(event);
+        //da error porque contador es una constante contador=contador +1;
+        console.log("reset entra:", contador)
+        actualizaContador(valor);
+        console.log("reset queda:", contador)
+    }  
     return (
     <>
     <h1> CounterApp</h1>
     <h2> {contador} </h2>
-    <button onClick={mifuncion}>{valor}</button>
+    <button onClick={sumar}>+1</button>
+    <button onClick={restar}>-1</button>
+    <button onClick={reset}>Reset</button>
     </>
     );
 }
