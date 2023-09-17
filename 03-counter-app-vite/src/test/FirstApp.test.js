2f02339f7f8214c926c91300db1ca6d923d75775
Óscar <rak0kso@gmail.com>
2023-09-17 13:57:35 +0200

    Tema 66 getByTestId

--- 03-counter-app-vite/src/test/FirstApp.test.js
+++ 03-counter-app-vite/src/test/FirstApp.test.js
@@ -11,6 +11,27 @@ describe('Pruebas en <FistApp />',() =>{
     test('debe de hacer match con el snapshot',() =>{
 
         const titulo = 'Hola, Soy Goku'
-        render (<FirstApp titulo={titulo}/>)
+        const {container } = render (<FirstApp titulo={titulo}/>);
+
+        //console.log(container);
+        expect ( container ).toMatchSnapshot();
+    })
+
+    test('debe mostrar el título en un h1',() =>{
+
+        const titulo = 'Hola, Soy Goku'
+        const {container, getByText, getByTestId } = render (<FirstApp titulo={titulo}/>);
+        //console.log(getByText(titulo));
+        //miramos que nuestro título este:
+        expect (getByText(titulo)).toBeTruthy();
+
+       
+        //miramos que el título esté en un h1, el primero
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toBe( titulo);
+
+        expect( getByTestId('test_1').innerHTML).toContain(titulo);
+
     })
 })
+
