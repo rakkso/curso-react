4c20ecb8c604f5141b55a429cf5939ae40a92248
Óscar <rak0kso@gmail.com>
2023-10-15 13:38:45 +0200

    Pruebas en GifIems y getGifs tema 104

--- 04-gifExpertApp-vite/tests/GifItem.test.js
+++ 04-gifExpertApp-vite/tests/GifItem.test.js
@@ -1,12 +1,14 @@
-import { render } from "@testing-library/react"
+import { render,screen } from "@testing-library/react"
 import GifItem from "../src/components/GifItem"
 
 describe('Pruebas de GifItem', () => {
-    
-    test('Debe hacer match con el snapshot', () => { 
-        
+ 
     const titulo ='Titulo ejemplo'
     const url = 'urlejemplo'
+
+    test('Debe hacer match con el snapshot', () => { 
+        
+ 
 
     
     const {container } = render (<GifItem title={titulo} url={url}/>);
@@ -14,6 +16,23 @@ describe('Pruebas de GifItem', () => {
     expect (container).toMatchSnapshot();
     })
     
-    
+    test('La url debe usarse en el img', () => { 
+     
+        render (<GifItem title={titulo} url={url}/>);
+
+        //screen.debug nos enseña el código html renderizado
+        screen.debug();
+        const { src, alt } =screen.getByRole('img');
+        expect (src).toBe("http://localhost/"+url);
+        expect (alt).toBe(titulo);
+
+        
+    })
+
+    test('debe mostrar el título en el componente ', () => { 
+        render (<GifItem title={titulo} url={url}/>);
+        expect( screen.getByText (titulo)).toBeTruthy();
+
+     })
 
 })
