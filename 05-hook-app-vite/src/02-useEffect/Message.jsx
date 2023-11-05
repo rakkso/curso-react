21b33059e92ad6dbc4e20d0596ef24d683b7e8f6
Óscar <rak0kso@gmail.com>
2023-11-05 20:25:54 +0100

    123 UseEffect llamadas

--- 05-hook-app-vite/src/02-useEffect/Message.jsx
+++ 05-hook-app-vite/src/02-useEffect/Message.jsx
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+
+    const [coords,setCoords] = useState({x:0,y:0});
+
     useEffect(() => {
+
+      const onMouseMove = ( {x,y}) => {
+        const coords = {x,y};
+        console.log( "Coordenadas:", coords);
+        setCoords({x,y});
+        
+      }
+    
       console.log ("Monando")
+      window.addEventListener('mousemove', onMouseMove)
     
       return () => {
         console.log ("DesMonando")
+        window.removeEventListener('mousemove',onMouseMove)
       }
     }, [])
     
   return (
     <>
     <h1>Unsername repetido</h1>
+    {JSON.stringify(coords)}
     </>
   )
 }
