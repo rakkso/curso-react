571abe53f5e7523132688e3519d981b57f3bb1e1
Óscar <rak0kso@gmail.com>
2023-12-10 18:19:57 +0100

    TEma 133 uso sin useMemo

--- 05-hook-app-vite/src/main.jsx
+++ 05-hook-app-vite/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import { MemoHook } from './06-memo/MemoHook'
 //import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
 //import Layout from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memo/Memorize'
+//import { Memorize } from './06-memo/Memorize'
 //import { FromWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { HooksApp } from './HooksApp'
 //import { CounterApp } from './01-useState/CounterApp'
@@ -14,6 +15,6 @@ import { Memorize } from './06-memo/Memorize'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   </React.StrictMode>,
 )
