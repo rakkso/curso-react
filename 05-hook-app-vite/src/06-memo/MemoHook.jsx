da7a29fa0683a5500e7bc23e110269ef8af0370e
Óscar <rak0kso@gmail.com>
2023-12-10 18:22:23 +0100

    Tema 133 usando useMemo

--- 05-hook-app-vite/src/06-memo/MemoHook.jsx
+++ 05-hook-app-vite/src/06-memo/MemoHook.jsx
@@ -14,13 +14,13 @@ export const MemoHook = () => {
     const {counter,increment} = useCounter(10);
     const [show, setshow] = useState(true);
 
-    //const memorizedValue = useMemo(() => heavyStuff, [counter])
+    const memorizedValue = useMemo(() => heavyStuff(counter), [counter])
   return (
     <>
 
         <h1>Counter: <small>{counter}</small></h1>
         <hr/>
-        <h4>{ heavyStuff(counter) } </h4>
+        <h4>{ memorizedValue } </h4>
         
         <button
             className='btn btn-primary'
