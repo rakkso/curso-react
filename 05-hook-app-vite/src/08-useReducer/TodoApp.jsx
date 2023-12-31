6664e129ea3a2799a57d717bc569943e82443a36
Óscar <rak0kso@gmail.com>
2023-12-31 10:51:11 +0100

    Sección 10 Tema 145 previo a la tarea

--- 05-hook-app-vite/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app-vite/src/08-useReducer/TodoApp.jsx
@@ -24,15 +24,38 @@ export const TodoApp = () => {
   return (
     <>
     
-        <h1>TodoApp</h1>
+        <h1>TodoApp: 10,<small> pendientes: 2</small></h1>
         <hr/>
-
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
-
+        <div className="row">
+          <div className='col-7'>
+            <ul className='list-group'>
+              {
+                todos.map( todo => (
+                  <li className='list-group-item d-flex justify-content-between'>
+                  <span className='align-self-center'>Item1</span>
+                  <button className='btn btn-danger'>Borrar</button>
+                  </li>
+    
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+        <div className='col-5'>
+          <h4>Agregar TODO</h4>
+          <hr/> 
+          <form>
+            <input
+            type='text'
+            placeholder='¿Qué ha que hacer?'
+            className='form-control'
+            />
+            <button
+            type='submit'
+            className='btn btn-outline-primary mt-1'
+            > Agregar </button>
+            </form> 
+        </div>
     </>
   )
 }
