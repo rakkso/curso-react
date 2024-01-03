8a8dcc3899b8b8d32916f0499588f2b500b0ffe6
Óscar <rak0kso@gmail.com>
2024-01-03 19:56:48 +0100

    Sección 11 TEma 157 configurando ReactRoure BrowserRouter

--- 05-hook-app-vite/src/main.jsx
+++ 05-hook-app-vite/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 //import { MemoHook } from './06-memo/MemoHook'
-import { CallBackHook } from './06-memo/CallBackHook'
-import { Padre } from './07-tarea-memo/Padre'
+//import { CallBackHook } from './06-memo/CallBackHook'
+//import { Padre } from './07-tarea-memo/Padre'
 //import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
 //import Layout from './05-useLayoutEffect/Layout'
@@ -16,10 +16,15 @@ import { Padre } from './07-tarea-memo/Padre'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 
 //import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp'
-
+//import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp />
-  </React.StrictMode>,
+  <BrowserRouter>
+  { /* <React.StrictMode> */}
+    <MainApp/>
+  { /* </React.StrictMode> */}
+  </BrowserRouter>
+    
+  
 )
