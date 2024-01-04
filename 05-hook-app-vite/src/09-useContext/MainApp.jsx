1fe55840930171a536be38bf194f23768eedc214
Óscar <rak0kso@gmail.com>
2024-01-04 10:07:14 +0100

    Sección 11 Configurar Router de React

--- 05-hook-app-vite/src/09-useContext/MainApp.jsx
+++ 05-hook-app-vite/src/09-useContext/MainApp.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { HomePage } from './homePage'
+import { LoginPage } from './LoginPage'
+import { AboutPage } from './AboutPage'
 
 export const MainApp = () => {
   return (
     <>
+    
      <h1>MainApp</h1>
      <hr/>
+
+     <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+      {/* <Route path="/*" element={<LoginPage/>}/> Todo lo lo definido a LoginPage pero sin navegación y en la siguente con navegación, cambia la url del navegador*/ }
+      <Route path="/*" element={<Navigate to="/about"/>} /> 
+     </Routes>
     </>
   )
 }
