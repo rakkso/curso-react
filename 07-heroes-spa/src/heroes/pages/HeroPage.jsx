9b6ad83971c58b72fd4ab64aa11f50b4d4057045
Óscar <rak0kso@gmail.com>
2024-03-30 16:55:16 +0100

    198 mirando el path de la url

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { HeroesList } from '../components/HeroesList'
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
+
 
 export const HeroPage = () => {
+
+  const {heroid}=useParams();
+  console.log(heroid);
+  const hero = getHeroById(heroid);
+
+  if (!hero) {
+    return <Navigate to="/marvel"/>
+  }
   return (
     <>
-    <h1>HeroPage</h1>
+    <h1>{heroid}</h1>
     
 </>
     
--- 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -13,7 +13,7 @@ export const HeroesRoutes = () => {
         <Route path="marvel" element={<MarvelPage/>}/>
         <Route path="dc" element={<DcPage/>}/>
         <Route path="search" element={<SearchPage/>}/>
-        <Route path="hero" element={<HeroPage/>}/>
+        <Route path="hero/:heroid" element={<HeroPage/>}/>
         <Route path="/" element={<Navigate to="/marvel" />}/>
     </Routes>
     </div>
