0ca79124c36566ee7f28eb3c176554fcad789301
Óscar <rak0kso@gmail.com>
2024-08-24 18:21:25 +0200

    Sección 17 Tema 263 crear snippet gists en visual studio b

--- 09-TOOLKIT-REDUX/src/App.jsx
+++ 09-TOOLKIT-REDUX/src/App.jsx
@@ -2,32 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {useDispatch,useSelector} from 'react-redux';
+import { increment,decrement,incrementBy } from './store/slices/counter';
 function App() {
   const [count, setCount] = useState(0)
 
+  const {contador} = useSelector (state => state.counter)
+  const dispatch = useDispatch();
+
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch (increment())}>
+          count is: {contador}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="card">
+        <button onClick={() => dispatch (decrement())}>
+          decrement
+        </button>
+        <button onClick={() => dispatch (incrementBy(5))}>
+          Increment by 5
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      </div>
+
     </>
   )
 }
