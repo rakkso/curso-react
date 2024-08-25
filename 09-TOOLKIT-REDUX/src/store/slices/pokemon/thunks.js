63c35473730606918d698a35c932e469c5773dd6
Óscar <rak0kso@gmail.com>
2024-08-25 14:00:53 +0200

    Sección 17 tema 266 con axios

--- 09-TOOLKIT-REDUX/src/store/slices/pokemon/thunks.js
+++ 09-TOOLKIT-REDUX/src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page =0 ) => {
@@ -9,8 +10,10 @@ export const getPokemons = (page =0 ) => {
 
         // TODO llamar a la pide
 
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page *10}`);
-        const data = await resp.json();
+        //const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page *10}`);
+       // const data = await resp.json();
+
+       const {data} = await pokemonApi.get(`pokemon?limit=10&offset=${page *10}`)
         console.log(data);
         //dispatch(setPokemons());
 
